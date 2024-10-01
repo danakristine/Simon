b52f2e8a5035ba569d82a3e68930718b6e9200e6
danakristine <2025miller.dana@fcboe.org>
2024-10-01 13:46:41 -0400

    Update simon.py
    one of the final edits (still have to finish doc strings!)

--- simon.py
+++ simon.py
@@ -42,15 +42,33 @@ start = False
 points = 0
 user_press = 1
 user_idx = 0
+game = []
 
+# creating functions!!
 
-# creating functions
 def add_to_sequence(lyst):
+    """
+    adds a random number bettwen 0-3 to the game list.
+
+    parameter:
+      lyst (list): the name of the list the random int will be added to
+
+    output:
+      prints out the list
+      
+    """
     lyst.append(random.randint(0, 3))
     print(lyst)
 
 
 def display_sequence(jist):
+    """
+    lights up the led associated with the random int in the game list
+        
+    parameter:
+       jist (list): the name of the list that is being iterated through
+       
+    """
     for item in jist:
         if item == 0:
             red.value = True
@@ -77,92 +95,121 @@ def display_sequence(jist):
             time.sleep(0.3)
 
 
-# def game_over:
+def game_over():
+    """
+    lights up each led and sets the game sequence to empty
 
-
-def display_scr():
-    print(points)
+    takes no args
+    
+    """
+    global game
     blue.value = True
+    green.value = True
     red.value = True
     yellow.value = True
-    green.value = True
-    time.sleep(0.2)
+    time.sleep(0.3)
+    game = []
     blue.value = False
+    green.value = False
     red.value = False
     yellow.value = False
-    green.value = False
-    time.sleep(0.2)
+    game_start = False
+    time.sleep(0.3)
+    display_scr(points)
+    print(points)
+
+
+
+def display_scr(users):
+    """
+    displays the value of the score
+    
+    parameter:
+    users (int): the number of points
+
+    """
+    print(users)
 
 
 def reset():
     global start
-    global mist
+    global game
     start = False
-    mist = []
+    game = []
 
 
 def user_updated():
     global user_press
     user_press = -1
-    if redb.value:
-        red.value = True
-        time.sleep(0.3)
-        red.value = False
-        time.sleep(0.3)
-        user_press = 0
-        return user_press
+    while not (blueb.value or redb.value or yellowb.value or greenb.value):
+        pass
+        if redb.value:
+            red.value = True
+            time.sleep(0.3)
+            red.value = False
+            time.sleep(0.3)
+            return 0
 
-    if greenb.value:
-        green.value = True
-        time.sleep(0.3)
-        green.value = False
-        time.sleep(0.3)
-        user_press = 1
-        return user_press
+        if greenb.value:
+            green.value = True
+            time.sleep(0.3)
+            green.value = False
+            time.sleep(0.3)
+            return 1
 
-    if yellowb.value:
-        yellow.value = True
-        time.sleep(0.3)
-        yellow.value = False
-        time.sleep(0.3)
-        user_press = 2
-        return user_press
+        if yellowb.value:
+            yellow.value = True
+            time.sleep(0.3)
+            yellow.value = False
+            time.sleep(0.3)
+            return 2
 
-    if blueb.value:
-        blue.value = True
-        time.sleep(0.3)
-        blue.value = False
-        time.sleep(0.3)
-        user_press = 3
-        return user_press
+        if blueb.value:
+            blue.value = True
+            time.sleep(0.3)
+            blue.value = False
+            time.sleep(0.3)
+            return 3
 
 
 def comparing_led():
+    global game
     global user_idx, user_press
     while not (blueb.value or redb.value or yellowb.value or greenb.value):
         pass
-    if blueb.value or redb.value or yellowb.value or greenb.value:
-        user_press = user_updated()
-        for i in range(len(mist)):
-            if mist[user_idx] == user_press:
-                user_idx += 1
-            else:
-                display_scr()
-        add_to_sequence(mist)
-        display_sequence(mist)
-            
+    user_press = user_updated()
+    for i in range(len(game)):
+        if game[user_idx] == user_press:
+            user_idx += 1
+        else:
+            print("wrong!!!")
+            user_idx = 0
+            game_over()
+        
+    
+def user_seq():
+    global user_idx
+    global game
+    return (user_idx == len(game))
+
+
+
 
 # main
-mist = []
 
 while True:
     if not start and whiteb.value:
         start = True
         time.sleep(0.5)
         if start:
-            add_to_sequence(mist)
-            display_sequence(mist)
+            add_to_sequence(game)
+            display_sequence(game)
             comparing_led()
+            if user_seq():
+                points += 1
+                time.sleep(1.0)
+
+
 
 
 
